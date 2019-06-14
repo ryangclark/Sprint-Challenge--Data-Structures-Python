e3cf127ba4119f6ab7c5525e813883bc04998793
Clark <rclark010@gmail.com>
2019-06-14 07:55:12 -0600

    Adds binary search tree to dramatically improve performance

--- names/names.py
+++ names/names.py
@@ -1,3 +1,4 @@
+from binary_search_tree import BinarySearchTree
 import time
 
 start_time = time.time()
@@ -10,13 +11,16 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+bst = BinarySearchTree(names_1[0])
+
+for name in names_1:
+    bst.insert(name)
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
-
